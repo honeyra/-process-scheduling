7bf9442589ec3cc26372a8c40469913060308667
Prakhar Rai <49952796+honeyra@users.noreply.github.com>
2019-04-25 18:57:09 +0530

    Update github.c

--- github.c
+++ github.c
@@ -104,6 +104,6 @@ for(i=0;i<n;i++)
 printf("P%d\t%d\t%d\t%d\t%d\n",p_list[i].p_no,p_list[i].arrival_t,p_list[i].ct,p_list[i].taround_time
 ,p_list[i].wait_t);
  }
-printf("Average Turn around Time: %f\t\n\n",avg_taround_time);
-printf("Average Waiting Time :\t %f\t\n",avg_w_t);
+printf("Average of Turn around Time: %f\t\n\n",avg_taround_time);
+printf("Average of Waiting Time :\t %f\t\n",avg_w_t);
 }
